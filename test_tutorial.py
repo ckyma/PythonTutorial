4459edffb6d13cb64b8df6d0cb5d3287489f1921
Yu Chen <ckyma@users.noreply.github.com>
2014-03-31 18:20:10 +0800

    add cases in Chapter 10

--- test_tutorial.py
+++ test_tutorial.py
@@ -434,3 +434,62 @@ for char in reverse('golf'):
 sum(i*i for i in range(10))
 #%%
 
+###############################################################################
+# Chapter 10. Brief Tour of the Standard Library
+
+###
+# 10.8. Dates and Times
+###
+#%%
+from datetime import date
+now = date.today()
+now
+#%%
+#%%
+birthday = date(1983, 7, 27)
+age = now - birthday
+age.days
+#%%
+
+###
+# 10.9. Data Compression
+###
+#%%
+from timeit import Timer
+Timer("t = a; a = b; b = t", "a = 1; b = 2").timeit()
+Timer("a, b = b, a", "a = 1; b = 2").timeit()
+#%%
+
+###
+# 10.11. Quality Control
+###
+# doctest in docstring
+#%%
+def average(values):
+    """Computes the arithmatic mean of a list of numbers.
+    
+    >>> print(average([20, 30, 70]))
+    40.0
+    """
+    return sum(values) / len(values)
+    
+import doctest
+doctest.testmod()
+#%%
+# unittest
+#%%
+import unittest
+
+class TestStaticalFunctions(unittest.TestCase):
+    
+    def test_average(self):
+        self.assertEqual(average([20, 30, 70]), 40.0)
+        self.assertEqual(round(average([1, 5, 7]), 1), 4.3)
+        with self.assertRaises(ZeroDivisionError):
+            average([])
+        with self.assertRaises(TypeError):
+            average(20, 30, 70)
+            
+unittest.main() # Calling from the command line invokes all tests
+
+#%%
