202d12e596b88d13a29677200aab789663739c08
Yu Chen <ckyma@users.noreply.github.com>
2014-04-07 13:23:12 +0800

    add more cases in Chapter 2 from Numpy and need merge with later changes from other source

--- test_numpy.py
+++ test_numpy.py
@@ -24,3 +24,70 @@ d = np.dtype(np.uint8)
 np.issubdtype(d, np.uint8)
 np.issubdtype(d, int)
 #%%
+
+###
+# 2.4.5 Indexing Multi-dimensional arrays
+###
+#%%
+y = np.arange(35).reshape(5, 7)
+#%%
+#%%
+y[np.array([0, 2, 4]), np.array([0, 1, 2])]
+#%%
+
+###
+# 2.4.8 Structural indexing tools
+###
+#%%
+x = np.arange(5)
+x[:, np.newaxis]
+x[np.newaxis, :]
+x[:, np.newaxis] + x[np.newaxis, :]
+#%%
+#%%
+z = np.arange(81).reshape(3, 3, 3, 3)
+z
+z[1, ..., 2]
+# equivalent to
+z[1, :, :, 2]
+#%%
+#%%
+x = np.arange(0, 50, 10)
+x
+x[np.array([1, 1, 3, 1])] += 1
+x
+#%%
+
+###
+# 2.4.10 Dealing with variable numbers of indices within programs
+###
+#%%
+indices = (1, 1, 1, slice(0, 2))
+z[indices]
+# equivalent to
+z[1, 1, 1, 0:2]
+
+indices = (1, Ellipsis, 1)
+z[indices]
+# equivalent to
+z[1, :, :, 1]
+#%%
+
+###
+# 2.5.1 General Broadcasting Rules
+
+###
+#%%
+x = np.arange(4)
+xx = x.reshape(4, 1)
+y = np.ones(5)
+xx + y
+#%%
+
+###
+# 2.6.1 Introduction to byte ordering and ndarrays
+###
+#%%
+big_end_str = chr(0) + chr(1) + chr(3) + chr(2)
+big_end_str
+#%%
